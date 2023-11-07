966e2aebd6033726dbfd387e5a3d9097ee3768d2
pya-h <p4ya.py@gmail.com>
2023-11-07 12:13:24 +0330

    updated readme

--- src/globals/redux/store/index.js
+++ src/globals/redux/store/index.js
@@ -4,11 +4,11 @@ import { reducers } from "../reducers";
 
 export const store = createStore(
     reducers,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__() //temp: enable chrome redux manager extension
-    )
+    // compose(
+    applyMiddleware(thunk),
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //     window.__REDUX_DEVTOOLS_EXTENSION__() //temp: enable chrome redux manager extension
+    // )
 );
 
 //dispatch
